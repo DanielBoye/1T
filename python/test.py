733b39371bcc111b1af2329be8567a708553912f
Daniel Boye <danielboye888@gmail.com>
2022-10-06 10:09:25 +0200

    .

---
+++ python/test.py
@@ -0,0 +1,5 @@
+a = int(input("Skriv inn tall: "))
+
+resultat = (a - 2) * 4 + 16 - (a * 4) 
+
+print(resultat)
---
+++ python/utforsk.py
@@ -0,0 +1,12 @@
+H = int(input("Skriv inn tall: "))
+H = test
+J = H - 2
+K = J * 4
+L = K + 16
+O = L - (H * 4)
+print(O)
+
+
+
+
+H + (((H - 2)*4)-16)-((test*4))        
