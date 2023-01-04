4974a9f873ec0b211171fecbf6c20b702a4c94a9
DanielBoye <83395536+DanielBoye@users.noreply.github.com>
2023-01-04 09:34:23 +0100

    Create lys.py

---
+++ naturfag/lys.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from pylab import *
+
+r = 1
+effekt = 100
+avstand_max = 15
+
+liste_r = []
+liste_intensitet = []
+
+def E(r):
+    intensitet = effekt * (1 / (r*r))
+    return intensitet
+
+while r <= avstand_max:
+    liste_r.append(r)
+    liste_intensitet.append(E(r))
+    r += 0.1
+
+plt.plot
+plt.xlabel("")
+plt.ylabel("Intensitet")
+plt.show()
